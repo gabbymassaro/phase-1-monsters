782ce690a897236f6b65786eae942ec0cc1a853d
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-19 21:06:23 -0400

    edit data.map to return whole object
    new data.map appends a new child each based on name, age, description

--- js/index.js
+++ js/index.js
@@ -7,15 +7,57 @@ fetch(`http://localhost:3000/monsters`)
     }
   })
   .then(data => {
-    const monsterNames = data.map((object) => {
-      return object.name
+
+    const monsters = data.map((object) => {
+      return object
     })
-    const headerName = document.getElementById('monstersAPI')
-    monsterNames.forEach(item => {
-      const h2 = document.createElement('h2');
-      h2.textContent = item
-      headerName.appendChild(h2)
+
+    const monsterBios = document.getElementById('monstersAPI')
+    monsters.forEach(item => {
+      const h2 = document.createElement('h2')
+      const h4 = document.createElement('h4')
+      const p = document.createElement('p')
+      h2.textContent = item.name
+      h4.textContent = item.age
+      p.textContent = item.description
+      monsterBios.appendChild(h2)
+      monsterBios.appendChild(h4)
+      monsterBios.appendChild(p)
     })
+
+    // const monsterNames = data.map((object) => {
+    //   return object.name
+    // })
+
+    // const monsterAges = data.map((object) => {
+    //   return object.age
+    // })
+
+    // const monsterBios = data.map((object) => {
+    //   return object.description
+    // })
+
+    // const headerName = document.getElementById('monstersAPI')
+    // monsterNames.forEach(item => {
+    //   const h2 = document.createElement('h2');
+    //   h2.textContent = item
+    //   headerName.appendChild(h2)
+    // })
+
+    // const headerAge = document.getElementById('monstersAPI')
+    // monsterAges.forEach(item => {
+    //   const h4 = document.createElement('h4');
+    //   h4.textContent = item
+    //   headerAge.appendChild(h4)
+    // })
+
+    // const headerBio = document.getElementById('monstersAPI')
+    // monsterBios.forEach(item => {
+    //   const h4 = document.createElement('h4');
+    //   h4.textContent = item
+    //   headerBio.appendChild(h4)
+    // })
+
   })
   .catch(error =>
     console.log(error)
