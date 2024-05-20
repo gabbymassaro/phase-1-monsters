82cedb5921cfaef2121b4ee70386d1048ecae9b5
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-19 21:06:53 -0400

    remove commented out code from first attempt

--- js/index.js
+++ js/index.js
@@ -24,40 +24,6 @@ fetch(`http://localhost:3000/monsters`)
       monsterBios.appendChild(h4)
       monsterBios.appendChild(p)
     })
-
-    // const monsterNames = data.map((object) => {
-    //   return object.name
-    // })
-
-    // const monsterAges = data.map((object) => {
-    //   return object.age
-    // })
-
-    // const monsterBios = data.map((object) => {
-    //   return object.description
-    // })
-
-    // const headerName = document.getElementById('monstersAPI')
-    // monsterNames.forEach(item => {
-    //   const h2 = document.createElement('h2');
-    //   h2.textContent = item
-    //   headerName.appendChild(h2)
-    // })
-
-    // const headerAge = document.getElementById('monstersAPI')
-    // monsterAges.forEach(item => {
-    //   const h4 = document.createElement('h4');
-    //   h4.textContent = item
-    //   headerAge.appendChild(h4)
-    // })
-
-    // const headerBio = document.getElementById('monstersAPI')
-    // monsterBios.forEach(item => {
-    //   const h4 = document.createElement('h4');
-    //   h4.textContent = item
-    //   headerBio.appendChild(h4)
-    // })
-
   })
   .catch(error =>
     console.log(error)
