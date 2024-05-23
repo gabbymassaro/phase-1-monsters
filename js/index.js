4c2320df5fe04570007079704e21d6065641c054
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-23 14:52:57 -0400

    Refactor old code, create POST, update DOM

--- js/index.js
+++ js/index.js
@@ -1,30 +1,50 @@
-fetch(`http://localhost:3000/monsters`)
-  .then(response => {
-    if (response.ok) {
-      return response.json()
-    } else {
-      throw new Error('API request failed')
-    }
-  })
-  .then(data => {
-
-    const monsters = data.map((object) => {
-      return object
+document.addEventListener("DOMContentLoaded", (e) => {
+  fetch(`http://localhost:3000/monsters/`)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error('API request failed')
+      }
+    })
+    .then(data => {
+        data.forEach(renderMonsters)
     })
 
-    const monsterBios = document.getElementById('monstersAPI')
-    monsters.forEach(item => {
-      const h2 = document.createElement('h2')
-      const h4 = document.createElement('h4')
-      const p = document.createElement('p')
-      h2.textContent = item.name
-      h4.textContent = `Age: ${item.age}`
-      p.textContent = `Bio: ${item.description}`
-      monsterBios.appendChild(h2)
-      monsterBios.appendChild(h4)
-      monsterBios.appendChild(p)
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let name = document.querySelectorAll("input")[0].value
+    let age = document.querySelectorAll("input")[1].value
+    let description = document.querySelectorAll("input")[2].value
+    const body = {name: name, age: age, description: description}
+
+    fetch('http://localhost:3000/monsters/', {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(body)
     })
+      .then(response => response.json())
+      .then(data => {
+        renderMonsters(data)
+      })
   })
-  .catch(error =>
-    console.log(error)
-  )
+  
+  const monsterBios = document.getElementById('monstersAPI')  
+  const renderMonsters = (monster) => {
+    const h2 = document.createElement('h2')
+    const h4 = document.createElement('h4')
+    const p = document.createElement('p')
+    h2.textContent = monster.name
+    h4.textContent = `Age: ${monster.age}`
+    p.textContent = `Bio: ${monster.description}`
+    monsterBios.appendChild(h2)
+    monsterBios.appendChild(h4)
+    monsterBios.appendChild(p)
+  }
+})
+
+
